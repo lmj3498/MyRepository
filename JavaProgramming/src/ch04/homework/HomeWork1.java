a76b499e269cb27d4fdd579b272d5ece4be74c97
lmj3498 <rhtnvlfem@naver.com>
2016-08-18 22:04:44 +0900

    숙제완료

--- JavaProgramming/src/ch04/homework/HomeWork1.java
+++ JavaProgramming/src/ch04/homework/HomeWork1.java
@@ -3,14 +3,14 @@ package ch04.homework;
 public class HomeWork1 {
 
 	public static void main(String[] args) {
-		int dice1 = 0;
-		int dice2 = 0;
-		int sum = 0;
-		for(int i=0;i<100;i++){
-			dice1 = (int)(Math.random()*6)+1;
-			dice2 = (int)(Math.random()*6)+1;
+		int dice1 = 0;//주사위1변수
+		int dice2 = 0;//주사위2변수
+		int sum = 0;//주사위 1,2의 합을 저장하기 위한 변수
+		for(int i=0;i<100;i++){//100번도는 for문
+			dice1 = (int)(Math.random()*6)+1;//1~6까지의 수를 주사위1 변수에 담음
+			dice2 = (int)(Math.random()*6)+1;//1~6까지의 수를 주사위2 변수에 담음
 			sum=dice1+dice2;
-			if(sum == 10){
+			if(sum == 10){//주사위1,2의 합이 10인 경우 주사위 숫자들을 출력하는 조건문
 				System.out.println("("+dice1+","+dice2+")");
 			}
 		}
--- JavaProgramming/src/ch04/homework/HomeWork2.java
+++ JavaProgramming/src/ch04/homework/HomeWork2.java
@@ -3,17 +3,34 @@ package ch04.homework;
 public class HomeWork2 {
 
 	public static void main(String[] args) {
-		int num1,num2;
-		int sum = 0;
+		//int num1,num2;
+		//int sum = 0;
 		
-		for(int i=1;i<=100;i++){
-			num1 = (int)(Math.random()*10)+1;
-			num2 = (int)(Math.random()*10)+1;
-			sum=(4*num1)+(5*num2);
-			if(sum == 60){
-				System.out.println("("+num1+","+num2+")");
+		//주어진 문제 해석1의 경우(이중 for문을 안쓰고자 했음)
+		for(int i=1;i<=10;i++){//1~10까지의 x범위에 대한 for문
+			if((60-4*i)%5 ==0){//4x+5y = 60 가 성립하기 위한 조건문
+				System.out.println("("+i+","+(60-4*i)/5+")");//x에 대한 값인 i와 성립된 식의 y값인 (60-4*i)/5의 몫을 출력함
 			}
 		}
+		
+		//이중포문을 쓴경우
+		/*for(int i=1;i<=10;i++){//1~10까지의 x범위에 대한 for문
+			for(int j=1;j<=10;j++){//1~10까지의 y범위에 대한 for문
+				if((4*i)+(5*j) == 60){//4x+5y = 60를 만족하는 숫자1,2를 출력하는 조건문
+					System.out.println("("+i+","+j+")");
+				}
+			}
+		}*/
+		
+		//주어진 문제 해석2(랜덤이라는 뜻)의 경우
+		/*for(int i=1;i<=100;i++){//100번이라는 임의의 횟수 
+			num1 = (int)(Math.random()*10)+1;//1~10범위의 랜덤 숫자를 숫자1에 담음
+			num2 = (int)(Math.random()*10)+1;//1~10범위의 랜덤 숫자를 숫자2에 담음
+			sum=(4*num1)+(5*num2); //4x+5y에 대한 연삽을 구함
+			if(sum == 60){//4x+5y = 60를 만족하는 숫자1,2를 출력하는 조건문
+				System.out.println("("+num1+","+num2+")");
+			}
+		}*/
 
 	}
 
