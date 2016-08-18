f355d9927c68bfec4cb5e3d2cecaf2e154a4442f
lmj3498 <rhtnvlfem@naver.com>
2016-08-18 18:56:22 +0900

    선생님 문제 파일

---
+++ JavaProgramming/src/ch04/homework/HomeWork1.java
@@ -0,0 +1,21 @@
+package ch04.homework;
+
+public class HomeWork1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+/*1) 주사위 2개를 100번 던집니다.
+두 주사위의 눈의 합이 10이 될 때
+"(주사위 1눈, 주사위2눈)"으로 출력하세요
+
+(5,5)
+(6,4)
+(4,6)
+...
+
+패키지명 : ch04.homework.HomeWork1*/
---
+++ JavaProgramming/src/ch04/homework/HomeWork2.java
@@ -0,0 +1,18 @@
+package ch04.homework;
+
+public class HomeWork2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+/*2) 4x+5y = 60;
+x의 범위는 1부터 10 사이의 정수
+y는 범위는 1부터 10 사이의 정수
+
+x가 5이면 y는 8 이고 이를 출력     
+
+
+패키지명 : ch04.homework.HomeWork2*/
