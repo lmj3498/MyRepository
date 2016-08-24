5f93f9c78f01bb8eaf953ff81b9201df169f07f4
lmj3498 <rhtnvlfem@naver.com>
2016-08-24 14:57:49 +0900

    중간 푸쉬

---
+++ JavaProgramming/src/ch06/exam09/Car.java
@@ -0,0 +1,38 @@
+package ch06.exam09;
+
+public class Car {
+	//필드
+	int gas;
+	
+	//생성자
+	
+	//메소드
+	void setGas(int gas){
+		this.gas = gas;
+	}
+	
+	boolean isLeftGas(){
+		if(gas == 0){
+			System.out.println("gas가 없습니다.");
+			return false;
+		}else{
+			System.out.println("gas가 있습니다.");
+			return true;
+		}
+	}
+	
+	void run(){
+		while(true){
+			if(gas>0){
+				System.out.println("멈춥니다.(gas잔량 : "+ gas+")");
+				gas -= 1;
+			}else{
+				//break;
+				System.out.println("멈춥니다.(gas잔량 : "+ gas+")");
+				return;
+			}
+		}
+		//System.out.println("멈춤");
+	}
+	
+}
