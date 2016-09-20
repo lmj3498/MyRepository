6b70441f1f5ab6276fd1b1a1f3795bbcf10107ca
lmj3498 <rhtnvlfem@naver.com>
2016-09-20 11:58:30 +0900

    2016.09.20 중간 커밋

---
+++ JavaProgramming/src/ch15/exam07/QueueExample.java
@@ -0,0 +1,33 @@
+package ch15.exam07;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+
+	public static void main(String[] args) {
+		Queue<Message> queue = new LinkedList<>();
+		
+		queue.offer(new Message("sendMail", "홍길동"));
+		queue.offer(new Message("sendSMS", "감자바"));
+		queue.offer(new Message("sendKakaotalk", "홍두께"));
+		
+		while(!queue.isEmpty()){
+			Message message  = queue.poll();
+			switch(message.command){
+			case "sendMail":
+				System.out.println(message.to +" 님에게 메일을 보냅니다.");
+				break;
+			
+			case "sendSMS":
+				System.out.println(message.to +" 님에게 SMS을 보냅니다.");
+				break;
+				
+			case "sendKakaotalk":
+				System.out.println(message.to +" 님에게 카톡을 보냅니다.");
+				break;
+			}
+		}
+	}
+
+}
