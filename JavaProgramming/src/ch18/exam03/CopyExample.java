5ce7ad3e9da8ce78ba96da5617737c0c7d8927f3
lmj3498 <rhtnvlfem@naver.com>
2016-09-20 16:46:34 +0900

    2016.09.20 수업 끝

---
+++ JavaProgramming/src/ch18/exam03/CopyExample.java
@@ -0,0 +1,29 @@
+package ch18.exam03;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CopyExample {
+
+	public static void main(String[] args) throws IOException {
+		InputStream is = new FileInputStream("src/ch18/exam03/12345.jpg");
+		OutputStream os = new FileOutputStream("src/ch18/exam03/test.jpg");
+		
+		byte[] values = new byte[1024];
+		
+		int byteNum = -1;
+		
+		while((byteNum = is.read(values))!=-1){
+			
+			os.write(values, 0, byteNum);
+		}
+		
+		is.close();
+		os.flush();
+		os.close();
+	}
+
+}
