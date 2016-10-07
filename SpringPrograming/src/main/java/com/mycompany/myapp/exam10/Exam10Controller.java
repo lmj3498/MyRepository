9a29faf776b1e733c57916015a9604aefb6b0ba6
lmj3498 <rhtnvlfem@naver.com>
2016-10-07 14:26:21 +0900

    2016.10.07 수업 끝

--- SpringPrograming/src/main/java/com/mycompany/myapp/exam10/Exam10Controller.java
+++ SpringPrograming/src/main/java/com/mycompany/myapp/exam10/Exam10Controller.java
@@ -1,5 +1,7 @@
 package com.mycompany.myapp.exam10;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,6 @@ public class Exam10Controller {
 	private static final Logger logger = LoggerFactory.getLogger(Exam10Controller.class);
 	
 	private G g;
-	
 	@Autowired
 	public void setG(G g){
 		logger.info("setG() 실행");
@@ -28,10 +29,29 @@ public class Exam10Controller {
 	}
 	
 	private Service service;
-	@Autowired
+	//@Autowired -- Type 으로 주입
+	//@Resource -- 등록이름으로 주입
+	@Resource(name ="serviceImpl1")
 	public void setService(Service service){
 		this.service = service;
 	}
+	
+	private I i;
+	@Autowired
+	public void setI(I i){
+		logger.info("setI() 실행");
+		this.i = i;
+	}
+	
+	private J j;
+	@Autowired
+	public void setJ(J j){
+		logger.info("setJ() 실행");
+		this.j = j;
+	}
+	
+	@Autowired
+	private K k;
 	
 	@RequestMapping("/index")
 	public String index(){
@@ -48,4 +68,20 @@ public class Exam10Controller {
 		return "redirect:/exam10/index";
 	}
 	
+	@RequestMapping("/method2")
+	public String method2(){
+		logger.info("method2 처리");
+		i.method();
+		j.method();
+		
+		return "redirect:/exam10/index";
+	}
+	
+	@RequestMapping("/method3")
+	public String method3(){
+		logger.info("method3 처리");
+		k.method();
+		
+		return "redirect:/exam10/index";
+	}
 }
